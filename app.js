b7291edf2d91c9ca6e18b21f489f6115d1c61579
Christian Smitherman <104788550+christian-js@users.noreply.github.com>
2022-08-10 14:27:04 -0600

    css changes

---
+++ app.js
@@ -0,0 +1,86 @@
+const form = document.querySelector('form');
+const serviceSelect = document.querySelector('#service-select');
+const servicePaths = document.querySelectorAll('.service');
+
+const paths = [
+    {
+        id: 'carpet-cleaning',
+        fn: function () {
+            // Contains all the checkboxes for the rooms
+            const roomBoxes = document.querySelectorAll(`#${this.id} > label`);
+
+            // When a checkbox is selected, activate any questions on the next level
+            for (let roomBox of roomBoxes) {
+                roomBox.addEventListener('input', () => {
+                    const checkbox = roomBox.firstElementChild;
+                    const nextLevel = roomBox.nextElementSibling;
+                    if (checkbox.checked) {
+                        nextLevel.classList.add('active');
+                    } else {
+                        nextLevel.classList.remove('active');
+                    }
+                })
+            }
+        }
+    },
+
+    {
+        id: 'tile-cleaning',
+        fn: function () {
+
+        }
+    },
+
+    {
+        id: 'carpet-stretching',
+        fn: function () {
+
+        }
+    }
+];
+
+const app = function () {
+    let servicePath = null; // Will contain <div> with class of "service" and id matching the service type
+
+
+    serviceSelect.addEventListener('input', () => {
+
+        const serviceType = serviceSelect.value;
+
+        // If the user has already selected an option then changed, remove the original path
+        if (servicePath) {
+            form.reset(); // Clear all fields the user had filled prior to changing
+            form.elements.service.value = serviceType; // To prevent the service value from becoming blank
+            servicePath.classList.remove('active');
+        }
+
+        // Find the service path with the service type
+        for (service of servicePaths) {
+            if (service.id === serviceType) {
+                servicePath = service;
+                break;
+            }
+        }
+
+        // Activate the service path
+        servicePath.classList.add('active');
+
+        // Activate all inputs
+        const activeInputs = document.querySelectorAll('div.service.active input');
+        for (let activeInput of activeInputs) {
+            activeInput.disabled = false;
+        }
+
+        // Disable all inputs not in the active path
+        const inactiveInputs = document.querySelectorAll('div.service:not(.active) input');
+        for (inactiveInput of inactiveInputs) {
+            inactiveInput.disabled = true;
+        }
+
+        const path = paths.find(path => path.id === servicePath.id);
+        path.fn();
+    });
+
+}
+
+app();
