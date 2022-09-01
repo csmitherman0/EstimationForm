42f0891f1b42ae4fe1999b5219bf03db57882d51
Christian Smitherman <104788550+christian-js@users.noreply.github.com>
2022-09-01 11:54:40 -0600

    update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true })); // Body-parsing middleware. The request body is undefined by default.
+app.use(express.static(path.join(__dirname, 'public'))); // For static assets
 
 
 /*****************
@@ -13,7 +16,7 @@ const app = express();
  */
 
 app.get('/', (req, res) => {
-    res.sendFile('form/index.html')
+    res.sendFile('form.html', { root: __dirname });
 })
 
 app.listen(PORT, () => {
