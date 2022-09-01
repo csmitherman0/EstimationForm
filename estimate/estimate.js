c156c979991f5b2bebf1e2f17d1d8ec6b5c1fdb2
Christian Smitherman <104788550+christian-js@users.noreply.github.com>
2022-09-01 13:55:53 -0600

    updates

--- estimate/estimate.js
+++ estimate/estimate.js
@@ -1,1 +1,20 @@
 const info = require('./data');
+
+
+
+module.exports = function (jobData) {
+    let total = 0;
+
+    if (jobData.service === 'carpet-cleaning') {
+        const { areas } = jobData;
+        if (areas.includes('bedrooms')) {
+            total += jobData['num-bedrooms'];
+
+            //ERROR ASSIGNMENT TO CONSTANT VARIABLE
+            // areas = areas.splice(areas.findIndex(e => e === 'bedrooms'), 1);
+        }
+        total = areas.length * 50;
+    }
+
+    return total;
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const estimate = require('./estimate/estimate');
 
 const PORT = 3000;
 
@@ -7,16 +8,24 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true })); // Body-parsing middleware. The request body is undefined by default.
 app.use(express.static(path.join(__dirname, 'public'))); // For static assets
+app.use(express.urlencoded({ extended: true })); // Body-parsing middleware. The request body is undefined by default.
 
 
 /*****************
  * 
  * BEGIN EXPRESS ROUTES
  * 
- */
+ *****************/
 
 app.get('/', (req, res) => {
     res.sendFile('form.html', { root: __dirname });
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const total = estimate(req.body);
+    console.log(total);
+    res.redirect('/')
 })
 
 app.listen(PORT, () => {
